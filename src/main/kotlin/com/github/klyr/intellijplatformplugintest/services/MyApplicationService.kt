ed2037dbc03f336904bbf30e8c98e1f22cfb0012
GitHub Action <action@github.com>
2020-07-29 15:37:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/klyr/intellijplatformplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.klyr.intellijplatformplugintest.services
+
+import com.github.klyr.intellijplatformplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/klyr/intellijplatformplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/klyr/intellijplatformplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.klyr.intellijplatformplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.klyr.intellijplatformplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
